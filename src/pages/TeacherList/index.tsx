163206eecb5d5405de187c26ef164b2f8b67c328
Beatriz Murase <biamurase@gmail.com>
2020-08-05 18:45:34 -0300

    feat: add select components

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,24 +3,47 @@ import PageHeader from "../../components/PageHeader";
 
 import "./styles.css";
 import TeacherItem from "../../components/TeacherItem";
+import Input from "../../components/Input";
+import Select from "../../components/Select";
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="weekday">Dia da semana</label>
-            <input type="text" id="weekday" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Física", label: "Física" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "História", label: "História" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Gramática", label: "Gramática" },
+              { value: "Química", label: "Química" },
+              { value: "Sociologia", label: "Sociologia" },
+              { value: "Filosofia", label: "Filosofia" },
+              { value: "Literatura", label: "Literatura" },
+              { value: "Redação", label: "Redação" },
+              { value: "Educação Física", label: "Educação Físicas" },
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={[
+              { value: "0", label: "Domingo" },
+              { value: "1", label: "Segunda-feira" },
+              { value: "2", label: "Terça-feira" },
+              { value: "3", label: "Quarta-feira" },
+              { value: "4", label: "Quinta-feira" },
+              { value: "5", label: "Sexta-feira" },
+              { value: "6", label: "Sábado" },
+            ]}
+          />
+          <Input name="time" label="Hora" type="time" />
         </form>
       </PageHeader>
 
